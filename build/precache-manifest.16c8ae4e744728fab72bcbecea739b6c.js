8da4eb274a5c3fa1eb303a57a9af8a5b9877305f
tjgillweb <tjgillweb@gmail.com>
2020-07-17 14:46:22 +0530

    Add build

---
+++ build/precache-manifest.16c8ae4e744728fab72bcbecea739b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c66d1c3d8cf419c309060f1014809c1",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "edd485650e828442d11e",
+    "url": "/tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "a2436778992cd372315b",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.b9926c31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edd485650e828442d11e",
+    "url": "/tic-tac-toe/static/js/main.7e48c1ad.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
