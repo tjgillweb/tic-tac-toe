059ec885a8bdc0ff57bd44599fb6b4afc850be52
tjgillweb <tjgillweb@gmail.com>
2020-08-23 01:27:55 +0530

    Add opponent move on click

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,20 +6,55 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      xIsNext: true,
+      numSquares: [0,1,2,3,4,5,6,7,8],
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    const numSquares = this.state.numSquares.slice();
     if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    const opponentMove = this.calculateOpponentMove(i)
+    squares[i] = numSquares[i] = 'X';
+    squares[opponentMove] = numSquares[opponentMove] = 'O';
+
     this.setState({
       squares: squares,
-      xIsNext: !this.state.xIsNext,
+      numSquares: numSquares,
     });
+    
+  }
+
+  calculateOpponentMove(i){
+    // here we calculate the position of other player which should be anything but not i
+    const numSquares = this.state.numSquares.slice();
+
+    //Empty position array will hold the number between 0 to 8 except i from which a random number will be selected which will be the other player's position on the board
+    let emptyPosArray
+
+    // if i is 0, it should be any random number between 1 to 8 [1,2,3,4,5,6,8]
+    if(i === 0){
+      emptyPosArray = numSquares.slice(i+1)
+    }
+    // if i is last element, it should be anywhere in [0,1,2,3,4,5,6,7]
+    else if( i === 8 ){
+      emptyPosArray = numSquares.slice(0, i)
+    }
+    // if i is any number in between, e.g. 5, then it should be [0,1,2,3,4,6,7,8]
+    else{
+      emptyPosArray = numSquares.slice(0, i).concat(numSquares.slice(i + 1))
+    }
+
+    // filter the empty position array to find out the positions that are not filled by X and O's .
+    const vacantSquares = emptyPosArray.filter(x => {
+       return (x !== 'X') && (x !== 'O')
+     })
+    // find out any random number other than X and O positions to fill the next move 
+    const opponentRandomMove = vacantSquares[Math.floor(Math.random() * vacantSquares.length)];
+    return opponentRandomMove
   }
 
   calculateWinner(squares) {
