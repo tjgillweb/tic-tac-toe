e428d860413f9178197b42e326db749f121d8d9c
tjgillweb <tjgillweb@gmail.com>
2020-08-24 01:09:31 +0530

    Add styles to Board squares

--- src/components/Board.js
+++ src/components/Board.js
@@ -96,23 +96,16 @@ class Board extends React.Component {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
     return (
-      <div>
-          <div>{status}</div>
-        <div className="board-row">
+      <div className="board-container">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
-        </div>
       </div>
     );
   }
