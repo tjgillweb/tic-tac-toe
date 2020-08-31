da54b6636000dbe00501df998e39793e784b0d8a
tjgillweb <tjgillweb@gmail.com>
2020-08-31 20:47:20 +0530

    update static content

---
+++ static/precache-manifest.9fbcef55e29c0bd950fc83075c3259b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40b11c2295fe2ccc15519db844bab14",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7895958a3e3e4c05a2d",
+    "url": "./static/css/main.6d61ce63.chunk.css"
+  },
+  {
+    "revision": "d1752d6ef1e9d6125361",
+    "url": "./static/js/2.dadb6417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dadb6417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7895958a3e3e4c05a2d",
+    "url": "./static/js/main.85c3ec3e.chunk.js"
+  },
+  {
+    "revision": "23bedb808eb06b70b3c7",
+    "url": "./static/js/runtime-main.1ddce69c.js"
+  }
+]);
